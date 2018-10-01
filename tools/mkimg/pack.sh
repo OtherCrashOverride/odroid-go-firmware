ba31df75032986bdddee4abac1b8e3287a983d3f
OtherCrashOverride <OtherCrashOverride@users.noreply.github.com>
2018-10-01 09:24:33 -0500

    Add phy_init_data.

--- tools/mkimg/pack.sh
+++ tools/mkimg/pack.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 TITLE="odroid-go-firmware-$(date +%Y%m%d).img"
-./mkimg "$TITLE" 0x1000 ../../build/bootloader/bootloader.bin 0x10000 ../../build/odroid-go-firmware.bin 0x8000 ../../build/partitions.bin
+./mkimg "$TITLE" 0x1000 ../../build/bootloader/bootloader.bin 0x10000 ../../build/odroid-go-firmware.bin 0x8000 ../../build/partitions.bin 0xf000 ../../build/phy_init_data.bin
